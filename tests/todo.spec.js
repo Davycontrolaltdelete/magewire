28153c8c02a1c805efe5084656e02acc09e63273
Michiel Gerritsen <michiel@controlaltdelete.nl>
2023-11-09 09:40:33 +0100

    WIP

--- tests/todo.spec.js
+++ tests/todo.spec.js
@@ -3,10 +3,8 @@ const { test, expect } = require('@playwright/test');
 test('test', async ({ page }) => {
     await page.goto('http://localhost:8080/magewire/examples');
 
-
-
     await page.getByPlaceholder('Task Description...').click();
-    await page.getByPlaceholder('Task Description...').fill('First tastk');
+    await page.getByPlaceholder('Task Description...').fill('First task');
     await page.getByPlaceholder('Task Description...').press('Enter');
     await page.getByRole('button', { name: 'Save' }).click();
     await page.getByPlaceholder('Task Description...').click();
@@ -15,11 +13,8 @@ test('test', async ({ page }) => {
     await page.getByRole('button', { name: 'Save' }).press('Enter');
     await page.getByRole('button', { name: 'Shuffle' }).click();
     // await page.getByRole('button', { name: 'Close message' }).click();
-    await expect(page.getByText('First tastk')).toBeVisible();
+    await expect(page.getByText('First task')).toBeVisible();
     await expect(page.getByText('Second task')).toBeVisible();
-
-
-
 
     await page.getByRole('button', { name: 'Shuffle' }).click();
     //     await page.getByRole('cell', { name: '2' }).toBeVisible();
@@ -28,14 +23,12 @@ test('test', async ({ page }) => {
     //     await page.getByRole('button', { name: '»' }).click();
     // await page.getByRole('cell', { name: '20', exact: true }).nth(1).toBeVisible();
 
-
     await page.getByPlaceholder('Search for something awesome...').click();
 
     // livewire auto update not working in tests
     //     await page.getByPlaceholder('Search for something awesome...').fill('test');
     // await page.getByRole('button', { name: 'Save' }).press('Enter');
     //    await expect(page.getByText(' random results found.')).toBeVisible();
-
 
     await page.getByPlaceholder('Firstname').click();
     await page.getByPlaceholder('Firstname').fill('Firstname');
@@ -45,7 +38,6 @@ test('test', async ({ page }) => {
     await page.getByPlaceholder('Email', { exact: true }).fill('Email@email.email');
     await page.getByRole('button', { name: 'Validate' }).click();
     await expect(page.getByText('Validation success!')).toBeVisible();
-
 
     await page.locator('#reacticon').getByText('1', { exact: true }).click();
     await page.getByRole('button', { name: '+1' }).click();
@@ -64,5 +56,4 @@ test('test', async ({ page }) => {
     await page.getByRole('button', { name: '−7' }).click();
     await expect(page.locator('#reacticon').getByText('15')).toBeVisible();
     await page.getByRole('button', { name: 'Shuffle' }).click();
-
 });
